35445c9cb1331481235395fb9dfa624c43a3db05
Brian Schlining <bschlining@gmail.com>
2025-08-28 08:16:53 -0700

    Added test for findByUri

--- src/test/java/org/mbari/vars/vampiresquid/sdk/r1/VampireSquidKiotaClientIT.java
+++ src/test/java/org/mbari/vars/vampiresquid/sdk/r1/VampireSquidKiotaClientIT.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class VampireSquidKiotaClientIT {
 
+
     @Test
     public void testAuth() {
         var client = new VampireSquidKiotaClient(URI.create("https://gehenna.shore.mbari.org/vam"), "foofoo");
@@ -22,6 +23,22 @@ public class VampireSquidKiotaClientIT {
         var cameraIds = client.findAllCameraIds().join();
         assertNotNull(cameraIds);
         assertFalse(cameraIds.isEmpty());
+    }
+
+    @Test
+    public void testFindByUri_NotFound() {
+        var client = new VampireSquidKiotaClient(URI.create("https://gehenna.shore.mbari.org/vam"));
+        var camera = client.findByUri(URI.create("urn:mbari:vars:camera:nonexistent")).join();
+        assertNull(camera);
+    }
+
+    @Test
+    public void testFindByUri() {
+        var client = new VampireSquidKiotaClient(URI.create("https://gehenna.shore.mbari.org/vam"));
+        var uri = URI.create("https://m3.shore.mbari.org/videos/t_M3/mezzanine/DocRicketts/2019/12/1234/D1234_20191216T143703Z_h264.mp4");
+        var camera = client.findByUri(uri).join();
+        assertNotNull(camera);
+        assertEquals(uri, camera.getUri());
     }
 
 }
